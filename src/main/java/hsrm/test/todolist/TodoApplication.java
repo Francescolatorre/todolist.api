9add68df4fee0ab3ec9eb48f8dd794deedf28da3
Francesco Giuseppe La Torre <francesco.latorre@maibornwolff.de>
2021-05-30 13:13:53 +0200

    Change CORS Header

--- src/main/java/hsrm/test/todolist/TodoApplication.java
+++ src/main/java/hsrm/test/todolist/TodoApplication.java
@@ -19,7 +19,7 @@ public class TodoApplication {
 		return new WebMvcConfigurerAdapter() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*");
+				registry.addMapping("/todo/*").allowedOrigins("*");
 			}
 		};
 	}
